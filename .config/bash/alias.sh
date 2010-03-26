fd2bd31fa8bd8d141278b194e219c22f4ba48851
ben <benjaminfranzke@googlemail.com>
2010-03-26 09:18:54 +0100

    add la alias to bash config

--- .config/bash/alias.sh
+++ .config/bash/alias.sh
@@ -7,6 +7,7 @@ alias gplay="gnome-mplayer"
 
 # list hidden files  - if directory do NOT show contents and dont display ".."
 alias lh="ls -d .[^.]*"
+alias la="ls -a"
 alias r="resize_to_min 100 35"
 alias vim="r; vim"
 alias man="r; man"
