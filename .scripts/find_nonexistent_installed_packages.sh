c980b61930cb4b8c73cfb671aebca26122ba931f
ben <benjaminfranzke@googlemail.com>
2010-03-26 16:35:23 +0100

    find_nonexistend.. replace $VARS with portageq`s

--- .scripts/find_nonexistent_installed_packages.sh
+++ .scripts/find_nonexistent_installed_packages.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+source /etc/make.conf
+installed=$(find "`portageq vdb_path`" -maxdepth 2 -mindepth 2 -printf "%P\n" | xargs qatom | awk '{print $1 "/" $2}')
 
-source "/etc/make.conf"
-installed=$(find /var/db/pkg -maxdepth 2 -mindepth 2 -printf "%P\n" | xargs qatom | awk '{print $1 "/" $2}')
+portdirs="$(portageq portdir) $(portageq portdir_overlay)"
 
 for package in ${installed}
 do
-	for overlay in "/usr/portage/" ${PORTDIR_OVERLAY}
+	for overlay in ${portdirs}:
 	do
 		# as soon as we find a package => got to next package in outer loop
 		[[ -d "${overlay}/${package}" ]] && continue 2;
