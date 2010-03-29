5b3a0ae9b5c09d99ca7e5a1bdc2fa1374fb11409
ben <benjaminfranzke@googlemail.com>
2010-03-29 23:14:02 +0200

    correct email in git config file

--- .config/git/configure.sh
+++ .config/git/configure.sh
@@ -4,4 +4,4 @@ git config --global branch.master.remote origin
 git config --global branch.master.merge refs/heads/master
 
 git config --global user.name ben
-git config --global user.emai benjaminfranzke@gmail.com
+git config --global user.email benjaminfranzke@googlemail.com
