05b59a1a65628185a44b17679e013fa3570594fe
ben <benjaminfranzke@gmail.com>
2010-03-29 21:57:16 +0200

    add a git configure scripts that sets up my env

---
+++ .config/git/configure.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git config --global branch.master.remote origin
+git config --global branch.master.merge refs/heads/master
+
+git config --global user.name ben
+git config --global user.emai benjaminfranzke@gmail.com
