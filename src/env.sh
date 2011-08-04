cb937b7ca4b5ac777a4a5374d3cbea018c455381
Benjamin Franzke <benjaminfranzke@googlemail.com>
2011-08-04 16:16:13 +0200

    Add src/env.sh

---
+++ src/env.sh
@@ -0,0 +1,23 @@
+doecho() {
+	echo $@
+	eval $@
+}
+
+home=~ben
+
+doecho export prefix=$home/.local
+
+doecho export 'ACLOCAL="aclocal -I '$prefix'/share/aclocal"'
+
+doecho export LD_LIBRARY_PATH="$prefix/lib"
+doecho export PKG_CONFIG_PATH=$LD_LIBRARY_PATH/pkgconfig
+#doecho export C_INCLUDE_PATH="$prefix/include"
+
+doecho export EGL_DRIVER=egl_dri2
+#doecho export EGL_DRIVER=egl_gallium
+
+doecho export EGL_LOG_LEVEL=debug
+#doecho export EGL_PLATFORM=wayland
+
+doecho export XDG_RUNTIME_DIR=$home/src
+doecho export SDL_VIDEODRIVER=wayland
