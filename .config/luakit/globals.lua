96cf8dd5278d31a1fcba67456fea23d5af026e87
Benjamin Franzke <benjaminfranzke@googlemail.com>
2012-02-09 16:12:56 +0100

    luakit: Set monospace font for gmail text areas

--- .config/luakit/globals.lua
+++ .config/luakit/globals.lua
@@ -2,3 +2,9 @@ dofile("/etc/xdg/luakit/globals.lua")
 
 globals.homepage = "http://google.de/"
 globals.scroll_step = 30
+
+domain_props = {
+	["mail.google.com"] = {
+		user_stylesheet_uri = "file://" ..luakit.data_dir .. "/styles/gmail.css"
+	},
+}
