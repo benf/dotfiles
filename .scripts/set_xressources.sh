9631c5c4462aae4691fc596b5014eb80b906f383
Benjamin Franzke <benjaminfranzke@googlemail.com>
2013-02-22 11:15:04 +0100

    set_xressources: Optimize for speed
    We're faster when concatenating first and executing xrdb only once.

--- .scripts/set_xressources.sh
+++ .scripts/set_xressources.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+#find ~/.config/ \
+#	-mindepth 2 -maxdepth 2 \
+#	-type f -iname "*.xdefaults" \
+#	-exec xrdb -merge {} \;
+
+# It is faster to concat first and execute xrdb only once.
 find ~/.config/ \
 	-mindepth 2 -maxdepth 2 \
 	-type f -iname "*.xdefaults" \
-	-exec xrdb -merge {} \;
+	-exec cat {} + | xrdb -merge
