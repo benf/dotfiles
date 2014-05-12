1d78a684b330e2de6b77923e888706d23af0601a
Benjamin Franzke <benjaminfranzke@googlemail.com>
2014-05-12 08:13:13 +0200

    awesome: focus&raise on resize/move
    This was missing while converting to awesome 3.5 config.

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -407,8 +407,8 @@ end
 
 clientbuttons = awful.util.table.join(
     awful.button({ }, 1, function (c) client.focus = c; c:raise() end),
-    awful.button({ modkey }, 1, awful.mouse.client.move),
-    awful.button({ modkey }, 3, awful.mouse.client.resize),
+    awful.button({ modkey }, 1, function(c) client.focus=c;c:raise(); awful.mouse.client.move(c); end),
+    awful.button({ modkey }, 3, function(c) client.focus=c;c:raise(); awful.mouse.client.resize(c) end),
     awful.button({ modkey }, 2, function(c) c:kill() end)
 )
 -- Set keys
